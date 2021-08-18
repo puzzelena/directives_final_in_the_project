8da0a958cfbe86b8292cf95b8c8b93b8e76bd8e8
puzzelena <puzic.elena1@gmail.com>
2021-08-18 15:41:23 +0300

    121. Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,27 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 export class RecipeService {
     // recipeService is working on mnaging parts of the recipe
     // therefore we go to recipe-list.component.ts and take the current recipes defined there
    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('Another Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+        new Recipe('Chocolate Cake', 
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tortor tellus, finibus eu suscipit vitae, semper non elit. Sed cursus quis enim et varius. Donec sem nibh, venenatis nec semper quis, vestibulum ac velit. Morbi fringilla orci quis posuere hendrerit. Ut non purus non sapien rutrum accumsan eget sed quam. ', 
+        'https://cdn.pixabay.com/photo/2016/11/22/18/52/cake-1850011_960_720.jpg', 
+        [
+            new Ingredient('chocolate', 300),
+            new Ingredient('flour', 500)
+        ]),
+        // we can add some ingredients for this recipe
+        // here we pass an array of ingredients in the recipe
+        new Recipe('Summer Ice Cream', 
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce tortor tellus, finibus eu suscipit vitae, semper non elit. Sed cursus quis enim et varius. Donec sem nibh, venenatis nec semper quis, vestibulum ac velit. Morbi fringilla orci quis posuere hendrerit. Ut non purus non sapien rutrum accumsan eget sed quam. ', 
+        'https://cdn.pixabay.com/photo/2019/07/28/04/15/sweets-4367900_960_720.jpg', 
+        [
+            new Ingredient('milk', 0.5),
+            new Ingredient('sugar', 100)
+        ])
       ];
 
       // we added private to not allow to get it from the outside
