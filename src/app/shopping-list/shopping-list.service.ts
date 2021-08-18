f6e256e491fa242be42b024a43eb5119df432552
puzzelena <puzic.elena1@gmail.com>
2021-08-18 15:09:58 +0300

    119. Adding the Shopping List Service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,21 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService {
-    
+    // first we copy all the ingredients from shopping-list.component.ts
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+      ];
+      // we made it private because we get data here in this array
+
+      getIngredients() {
+          return this.ingredients.slice();
+          // slice method doesnt get an acces to an original store of this array    
+      }
+
+      addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient)
+      }
+
+      
 }
